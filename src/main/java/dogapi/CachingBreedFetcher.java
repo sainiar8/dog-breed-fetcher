8331eef0c3fd72e7f8564d4accb385fbb48374f4
Arshdeep <arshdeep.saini@mail.utoronto.ca>
2025-10-17 09:23:32 -0400

    Completed tasks 2-4

--- src/main/java/dogapi/CachingBreedFetcher.java
+++ src/main/java/dogapi/CachingBreedFetcher.java
@@ -13,16 +13,30 @@ import java.util.*;
  * The cache maps the name of a breed to its list of sub breed names.
  */
 public class CachingBreedFetcher implements BreedFetcher {
-    // TODO Task 2: Complete this class
+    // Task 2: Complete this class
+    private BreedFetcher fetcher;
     private int callsMade = 0;
-    public CachingBreedFetcher(BreedFetcher fetcher) {
+    private Map<String, List<String>> cache = new HashMap<>();
 
+    public CachingBreedFetcher(BreedFetcher fetcher) {
+        this.fetcher = fetcher;
     }
 
     @Override
-    public List<String> getSubBreeds(String breed) {
-        // return statement included so that the starter code can compile and run.
-        return new ArrayList<>();
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException {
+        if (cache.containsKey(breed)) {
+            return cache.get(breed);
+        }
+
+        callsMade++;
+
+        try {
+            List<String> subBreeds = fetcher.getSubBreeds(breed);
+            cache.put(breed, subBreeds);
+            return subBreeds;
+        } catch (BreedNotFoundException e) {
+            throw new BreedNotFoundException(breed);
+        }
     }
 
     public int getCallsMade() {
--- src/main/java/dogapi/DogApiBreedFetcher.java
+++ src/main/java/dogapi/DogApiBreedFetcher.java
@@ -25,7 +25,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
      * @throws BreedNotFoundException if the breed does not exist (or if the API call fails for any reason)
      */
     @Override
-    public List<String> getSubBreeds(String breed) {
+    public List<String> getSubBreeds(String breed) throws BreedNotFoundException{
         // Task 1: Complete this method based on its provided documentation
         // and the documentation for the dog.ceo API. You may find it helpful
         // to refer to the examples of using OkHttpClient from the last lab,
@@ -51,7 +51,7 @@ public class DogApiBreedFetcher implements BreedFetcher {
                 throw new BreedNotFoundException("Breed not found: " + breed);
             }
         } catch (IOException | JSONException event) {
-            throw new RuntimeException(event);
+            throw new RuntimeException();
         }
 
         return subBreeds;
